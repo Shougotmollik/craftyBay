b263bcd4a38854bbc0299086e01cf76d62caabda
ShougotMollik <shougotmollik@gmail.com>
2024-09-08 22:29:17 +0600

    Home Screen ui => Product list

--- lib/presentation/ui/utils/assets_path.dart
+++ lib/presentation/ui/utils/assets_path.dart
@@ -3,4 +3,8 @@ class AssetsPath {
 //   All Logo here
 static const String appLogo="assets/logos/logo.svg";
 static const String appLogoNav="assets/logos/logo_nav.svg";
+
+// All Images
+
+static const String shoeImage="assets/images/shoe.png";
 }
