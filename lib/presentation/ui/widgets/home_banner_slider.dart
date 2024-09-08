5484bf7d97e1cb8062c8981d344f9a8f38b14ef1
ShougotMollik <shougotmollik@gmail.com>
2024-09-08 14:40:24 +0600

    Home Screen ui

---
+++ lib/presentation/ui/widgets/home_banner_slider.dart
@@ -0,0 +1,70 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:craftybay/presentation/ui/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class HomeBannerSlider extends StatefulWidget {
+  const HomeBannerSlider({
+    super.key,
+  });
+
+  @override
+  State<HomeBannerSlider> createState() => _HomeBannerSliderState();
+}
+
+class _HomeBannerSliderState extends State<HomeBannerSlider> {
+  final ValueNotifier<int> _selectedIndex = ValueNotifier(0);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+            height: 180.0,
+            onPageChanged: (index, reason) => _selectedIndex.value = index,
+          ),
+          items: [1, 2, 3, 4, 5].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration:  BoxDecoration(
+                      color: AppColors.primaryColor,
+                      borderRadius:BorderRadius.circular(8)
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'text $i',
+                      style: const TextStyle(fontSize: 16.0),
+                    ));
+              },
+            );
+          }).toList(),
+        ),
+        const SizedBox(height: 8),
+        ValueListenableBuilder(
+          valueListenable: _selectedIndex,
+          builder: (context, value, child) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (int i = 0; i < 5; i++)
+                  Container(
+                    height: 12,
+                    width: 12,
+                    margin: const EdgeInsets.only(left: 4),
+                    decoration: BoxDecoration(
+                      color: value == i
+                          ? AppColors.primaryColor
+                          : Colors.grey.shade300,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  )
+              ],
+            );
+          },
+        )
+      ],
+    );
+  }
+}
