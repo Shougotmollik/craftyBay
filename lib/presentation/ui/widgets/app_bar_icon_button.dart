cafcfbf66dedf41073dff34c3185dae198760309
ShougotMollik <shougotmollik@gmail.com>
2024-09-08 14:40:19 +0600

    Home Screen ui

---
+++ lib/presentation/ui/widgets/app_bar_icon_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppBarIconButton extends StatelessWidget {
+  const AppBarIconButton({
+    super.key, required this.onTap, required this.iconData,
+  });
+
+  final VoidCallback onTap;
+  final IconData iconData;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: CircleAvatar(
+          radius: 18,
+          backgroundColor: Colors.grey.shade200,
+          child:  Icon(
+            iconData,
+            color: Colors.grey,
+            size: 22,
+          )),
+    );
+  }
+}
