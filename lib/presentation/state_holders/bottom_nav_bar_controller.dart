f57021d211207ffcbd0a43da4493a0d951bd84c5
ShougotMollik <shougotmollik@gmail.com>
2024-09-11 15:58:22 +0600

    Home Screen ui => Product list, Bottom nav bar

---
+++ lib/presentation/state_holders/bottom_nav_bar_controller.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+
+class BottomNavBarController extends GetxController {
+  int _selectedIndex = 0;
+
+  int get selectedIndex => _selectedIndex;
+
+  void changeIndex(int index){
+    _selectedIndex=index;
+    update();
+  }
+  void changeCategory(){
+    changeIndex(1);
+  }void backToHome(){
+    changeIndex(0);
+  }
+}
