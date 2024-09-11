ad9d3da20b78a38bce6f2fcad70e72d8b76622f4
ShougotMollik <shougotmollik@gmail.com>
2024-09-11 15:58:37 +0600

    Home Screen ui => Product list, Bottom nav bar

--- lib/presentation/ui/widgets/category_card.dart
+++ lib/presentation/ui/widgets/category_card.dart
@@ -1,5 +1,7 @@
+import 'package:craftybay/presentation/ui/screens/navigation_screen/product_list_screen.dart';
 import 'package:craftybay/presentation/ui/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CategoryCard extends StatelessWidget {
   const CategoryCard({
@@ -8,25 +10,30 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            color: AppColors.primaryColor.withOpacity(0.1),
-            borderRadius: BorderRadius.circular(8),
+    return GestureDetector(
+      onTap: () {
+        Get.to(() => const ProductListScreen(categoryName: "Electronics"));
+      },
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: AppColors.primaryColor.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: const Icon(
+              Icons.computer,
+              color: AppColors.primaryColor,
+              size: 48,
+            ),
           ),
-          child: const Icon(
-            Icons.computer,
-            color: AppColors.primaryColor,
-            size: 48,
-          ),
-        ),
-        const Text(
-          "Electronics",
-          style: TextStyle(color: AppColors.primaryColor),
-        )
-      ],
+          const Text(
+            "Electronics",
+            style: TextStyle(color: AppColors.primaryColor),
+          )
+        ],
+      ),
     );
   }
 }
