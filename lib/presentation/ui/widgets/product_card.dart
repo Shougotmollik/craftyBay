a98b288fcf36f2b69d476dabe5c292503f84d2f5
ShougotMollik <shougotmollik@gmail.com>
2024-09-13 22:09:09 +0600

    Home Screen ui => Product Details

--- lib/presentation/ui/widgets/product_card.dart
+++ lib/presentation/ui/widgets/product_card.dart
@@ -1,6 +1,8 @@
+import 'package:craftybay/presentation/ui/screens/navigation_screen/product_details_screen.dart';
 import 'package:craftybay/presentation/ui/utils/assets_path.dart';
 import 'package:craftybay/presentation/ui/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProductCard extends StatelessWidget {
   const ProductCard({
@@ -9,80 +11,85 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.white,
-      elevation: 3,
-      child: SizedBox(
-        width: 140,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: 140,
-              height: 100,
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: AppColors.primaryColor.withOpacity(0.1),
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(8),
-                  topRight: Radius.circular(8),
+    return GestureDetector(
+      onTap: () {
+        Get.to(()=>const ProductDetailsScreen());
+      },
+      child: Card(
+        color: Colors.white,
+        elevation: 3,
+        child: SizedBox(
+          width: 140,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 140,
+                height: 100,
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: AppColors.primaryColor.withOpacity(0.1),
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(8),
+                    topRight: Radius.circular(8),
+                  ),
+                  image: const DecorationImage(
+                      image: AssetImage(AssetsPath.shoeImage),
+                      fit: BoxFit.scaleDown),
                 ),
-                image: const DecorationImage(
-                    image: AssetImage(AssetsPath.shoeImage),
-                    fit: BoxFit.scaleDown),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'New Year Special Shoe',
-                    maxLines: 1,
-                    style: TextStyle(
-                        fontWeight: FontWeight.w500, color: Colors.black54),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        '\$120',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            color: AppColors.primaryColor),
-                      ),
-                      const Wrap(
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        children: [
-                          Icon(Icons.star, color: Colors.amber),
-                          Text(
-                            '4.8',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                color: Colors.black54),
-                          ),
-                        ],
-                      ),
-                      Card(
-                        color: AppColors.primaryColor,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(4)),
-                        child: const Padding(
-                          padding: EdgeInsets.all(4.0),
-                          child: Icon(
-                            Icons.favorite_outline_rounded,
-                            size: 16,
-                            color: Colors.white,
-                          ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'New Year Special Shoe',
+                      maxLines: 1,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500, color: Colors.black54),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          '\$120',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              color: AppColors.primaryColor),
                         ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            )
-          ],
+                        const Wrap(
+                          crossAxisAlignment: WrapCrossAlignment.center,
+                          children: [
+                            Icon(Icons.star, color: Colors.amber),
+                            Text(
+                              '4.8',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black54),
+                            ),
+                          ],
+                        ),
+                        Card(
+                          color: AppColors.primaryColor,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4)),
+                          child: const Padding(
+                            padding: EdgeInsets.all(4.0),
+                            child: Icon(
+                              Icons.favorite_outline_rounded,
+                              size: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
