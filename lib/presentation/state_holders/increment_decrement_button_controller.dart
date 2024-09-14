a10e7d31fde955a58332ec2e56718212f872db41
ShougotMollik <shougotmollik@gmail.com>
2024-09-14 20:47:44 +0600

    Home Screen ui => Product Details

--- lib/presentation/state_holders/increment_decrement_button_controller.dart
+++ lib/presentation/state_holders/increment_decrement_button_controller.dart
@@ -1,12 +1,21 @@
 import 'package:get/get.dart';
 
 class IncrementDecrementButtonController extends GetxController {
-  int _initialValue = 1;
+  int _initialValue = 01;
 
   int get initialValue => _initialValue;
 
   void increment() {
-    _initialValue++;
-    update();
+    if (_initialValue < 20) {
+      _initialValue++;
+      update();
+    }
+  }
+
+  void decrement() {
+    if (_initialValue > 00) {
+      _initialValue--;
+      update();
+    }
   }
 }
