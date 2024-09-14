10b84b5e5375a7894f4d02c49f254f15285bfa68
ShougotMollik <shougotmollik@gmail.com>
2024-09-14 20:47:40 +0600

    Home Screen ui => Product Details

--- lib/presentation/ui/widgets/increment_decrement_button.dart
+++ lib/presentation/ui/widgets/increment_decrement_button.dart
@@ -1,4 +1,5 @@
 import 'package:craftybay/presentation/state_holders/increment_decrement_button_controller.dart';
+import 'package:craftybay/presentation/ui/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,20 +12,56 @@ class IncrementDecrementButton extends StatefulWidget {
 }
 
 class _IncrementDecrementButtonState extends State<IncrementDecrementButton> {
-
-  final IncrementDecrementButtonController _buttonController=Get.find<IncrementDecrementButtonController>();
+  final IncrementDecrementButtonController _buttonController =
+      Get.put(IncrementDecrementButtonController());
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
-      width: 200,
-      child: Row(
+      color: Colors.white,
+      height: 40,
+      width: 100,
+      child: Wrap(
+        alignment: WrapAlignment.spaceBetween,
+        crossAxisAlignment: WrapCrossAlignment.center,
         children: [
-          IconButton(onPressed: () {
-            _buttonController.increment;
-          }, icon: const Icon(Icons.remove)),
-          Text(_buttonController.initialValue.toString()),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.add))
+          Container(
+            height: 35,
+            width: 35,
+            decoration: BoxDecoration(
+                color: AppColors.primaryColor.withOpacity(0.75),
+                borderRadius: BorderRadius.circular(8)),
+            child: IconButton(
+                onPressed: _buttonController.decrement,
+                icon: const Icon(
+                  Icons.remove,
+                  size: 22,
+                  color: Colors.white,
+                )),
+          ),
+          GetBuilder<IncrementDecrementButtonController>(builder: (context) {
+            return Text(
+              "${_buttonController.initialValue}",
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
+            );
+          }),
+          Container(
+            height: 35,
+            width: 35,
+            decoration: BoxDecoration(
+                color: AppColors.primaryColor.withOpacity(0.75),
+                borderRadius: BorderRadius.circular(8)),
+            child: IconButton(
+              onPressed: _buttonController.increment,
+              icon: const Icon(
+                Icons.add,
+                size: 22,
+                color: Colors.white,
+              ),
+            ),
+          )
         ],
       ),
     );
