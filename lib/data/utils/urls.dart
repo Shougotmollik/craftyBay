085c3347d01db7f5b1554b80e06e61d374d8021b
ShougotMollik <shougotmollik@gmail.com>
2024-10-08 21:06:08 +0600

    api integration

--- lib/data/utils/urls.dart
+++ lib/data/utils/urls.dart
@@ -1,4 +1,5 @@
 class Urls {
   static const String _baseUrl = 'https://ecommerce-api.codesilicon.com/api';
   static const String dummyUrls = '$_baseUrl/login';
+  static const String sliderListUrl = '$_baseUrl/ListProductSlider';
 }
