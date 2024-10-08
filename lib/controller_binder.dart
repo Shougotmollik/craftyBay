869f740b6b902b864dae8f028dd28fa6b61f2280
ShougotMollik <shougotmollik@gmail.com>
2024-10-08 21:05:48 +0600

    api integration

--- lib/controller_binder.dart
+++ lib/controller_binder.dart
@@ -1,6 +1,7 @@
 import 'package:craftybay/data/services/network_caller.dart';
 import 'package:craftybay/presentation/state_holders/bottom_nav_bar_controller.dart';
 import 'package:craftybay/presentation/state_holders/increment_decrement_button_controller.dart';
+import 'package:craftybay/presentation/state_holders/slider_list_controller.dart';
 import 'package:get/get.dart';
 
 class ControllerBinder extends Bindings {
@@ -9,5 +10,6 @@ class ControllerBinder extends Bindings {
     Get.lazyPut(() => BottomNavBarController());
     Get.lazyPut(() => IncrementDecrementButtonController());
     Get.put(NetworkCaller());
+    Get.lazyPut(() => SliderListController());
   }
 }
