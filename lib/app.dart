cb920b6907fc276404ddb4576d5f5ecd01b2d8b2
ShougotMollik <shougotmollik@gmail.com>
2025-01-07 22:19:40 +0600

    - recreating the project

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,11 @@
-import 'package:craftybay/controller_binder.dart';
-import 'package:craftybay/presentation/ui/screens/auth_screen/splash_screen.dart';
-import 'package:craftybay/presentation/ui/utils/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 class CraftyBayApp extends StatelessWidget {
   const CraftyBayApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      themeMode: ThemeMode.system,
-      theme: AppTheme.lightTheme,
-      initialBinding: ControllerBinder(),
-      home: const SplashScreen(),
-    );
+    return const GetMaterialApp();
   }
 }
